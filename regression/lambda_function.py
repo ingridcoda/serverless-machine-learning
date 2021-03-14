06942e96a7617cad94ac3807bf9eef965bcb5dfa
Ingrid Coda <ingrid.coda@zoop.com.br>
2021-03-14 01:14:38 -0300

    More docker adjusts

--- regression/lambda_function.py
+++ regression/lambda_function.py
@@ -1,6 +1,17 @@
-def main(event, context):
-    print("Hello, World!")
+import json
+
+
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "Message": "Hello, World!"
+        })
+    }
 
 
 if __name__ == "__main__":
-    main('', '')
+    handler('', '')
